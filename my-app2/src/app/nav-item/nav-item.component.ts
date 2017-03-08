eb446ccc2c8d0bf4dd1fd3d5dc690c903df4f01f
erezam <amir4246@gmail.com>
2017-03-08 17:19:46 +0200

    NAV

---
+++ my-app2/src/app/nav-item/nav-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-item',
+  templateUrl: './nav-item.component.html',
+  styleUrls: ['./nav-item.component.css']
+})
+export class NavItemComponent implements OnInit {
+  @Input() item;
+
+  myFunc(id){
+    alert(id);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
